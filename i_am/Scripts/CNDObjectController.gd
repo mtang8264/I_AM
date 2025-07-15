7ce243ce387e15655d83df2ca055fce3486f8e9e
Michael Poltronieri Tang <mtang8264@gmail.com>
2025-07-15 11:16:45 -0400

    Added the ability to drag objects

--- i_am/Scripts/CNDObjectController.gd
+++ i_am/Scripts/CNDObjectController.gd
@@ -9,19 +9,23 @@ var bound_right: float
 var bound_top: float
 var bound_bottom: float
 
+var held: bool = false
+var held_starting_mouse_offset: Vector2 = Vector2.ZERO
+
 func _ready():
 	assign_child_variables()
 	calculate_bounds()
 
 func _process(delta):
+	drag()
 	calculate_bounds()
+	if not Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		release()
 
 func _input(event):
-	if event is InputEventMouseButton:
-		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and event.as_text() == "Left Mouse Button":
-			if is_point_inbounds(event.position.x, event.position.y):
-				print("Inbound touch on obj ", name, " at position ", event.position)
-			#print(event.position)
+	if is_input_event_mouse_down_on_obj(event):
+		#print("Inbound mousedown on obj ", name, " at position ", event.position)
+		hold(event.position)
 
 func assign_child_variables():
 	body = find_children("*", "StaticBody2D")[0]
@@ -37,5 +41,28 @@ func calculate_bounds():
 	bound_bottom = position.y + rect.end.y
 	#print(bound_left, bound_right, bound_top, bound_bottom)
 
-func is_point_inbounds(x: float, y: float):
-	return x >= bound_left and x <= bound_right and y >= bound_top and y <= bound_bottom
+func is_point_inbounds(point: Vector2):
+	return point.x >= bound_left and point.x <= bound_right and point.y >= bound_top and point.y <= bound_bottom
+
+func is_input_event_mouse_down_on_obj(event):
+	if event is InputEventMouseButton:
+		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and event.as_text() == "Left Mouse Button":
+			if is_point_inbounds(event.position):
+				return true
+	return false
+
+func hold(mouse_pos: Vector2):
+	# Set the held variable to true
+	held = true
+	# Record the mouse position at the moment the object begins being held
+	held_starting_mouse_offset = position - mouse_pos
+
+func release():
+	held = false
+	held_starting_mouse_offset = Vector2.ZERO
+
+func drag():
+	if not held:
+		return
+	var mouse_pos = get_viewport().get_mouse_position()
+	position = mouse_pos + held_starting_mouse_offset

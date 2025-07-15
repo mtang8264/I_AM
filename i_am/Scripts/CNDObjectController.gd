594b1d3692ad598547a66495719feb1bfdfa6841
Michael Poltronieri Tang <mtang8264@gmail.com>
2025-07-15 11:53:04 -0400

    Changed several comments into descriptions.

--- i_am/Scripts/CNDObjectController.gd
+++ i_am/Scripts/CNDObjectController.gd
@@ -31,14 +31,14 @@ func _input(event):
 		#print("Inbound mousedown on obj ", name, " at position ", event.position)
 		hold(event.position)
 
-# Finds the child nodes required for script to function
+## Finds the child nodes required for script to function
 func assign_child_variables():
 	body = find_children("*", "StaticBody2D")[0]
 	#print(body)
 	shape = find_children("*", "CollisionShape2D")[0]
 	#print(shape)
 
-# Calculates the click box bounds based on the shape assigend to the child CollisionShape2D
+## Calculates the click box bounds based on the shape assigend to the child CollisionShape2D
 func calculate_bounds():
 	var rect = shape.shape.get_rect()
 	bound_left = position.x + rect.position.x
@@ -47,11 +47,11 @@ func calculate_bounds():
 	bound_bottom = position.y + rect.end.y
 	#print(bound_left, bound_right, bound_top, bound_bottom)
 
-# Returns true if the given point is within bounds of the object
+## Returns true if the given point is within bounds of the object
 func is_point_inbounds(point: Vector2):
 	return point.x >= bound_left and point.x <= bound_right and point.y >= bound_top and point.y <= bound_bottom
 
-# Returns true if the given event is a left mouse click inside the bounds of the click box
+## Returns true if the given event is a left mouse click inside the bounds of the click box
 func is_input_event_left_mouse_down_on_obj(event):
 	if event is InputEventMouseButton:
 		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and event.as_text() == "Left Mouse Button":
@@ -59,20 +59,20 @@ func is_input_event_left_mouse_down_on_obj(event):
 				return true
 	return false
 
-# Void function used to mark the object as held
+## Void function used to mark the object as held
 func hold(mouse_pos: Vector2):
 	# Set the held variable to true
 	held = true
 	# Record the mouse position at the moment the object begins being held
 	held_starting_mouse_offset = position - mouse_pos
 
-# Void function used to release the obect from being held
+## Void function used to release the obect from being held
 func release():
 	held = false
 	held_starting_mouse_offset = Vector2.ZERO
 
-# Moves the object to the correct position given the mouse's new position
 func drag():
+## Moves the object to the correct position given the mouse's new position
 	if not held:
 		return
 	var mouse_pos = get_viewport().get_mouse_position()
